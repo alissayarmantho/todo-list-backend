7ad462ff630d37e2815c646360db9d9b4bdcdc49
Alissa <alissayarmantho1@gmail.com>
2020-12-29 22:58:50 +0800

    API Changed
    -Changed some parts of the API to handle query strings in todo controller
    
    -Changed the def show function in category controller to show the category instead of todo

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -4,37 +4,35 @@ module Api
             skip_before_action :verify_authenticity_token, :only => [:create, :update]
             def index
                 categories = Category.order('created_at');
-                render json: {status: 'SUCCESS', message: 'Loaded all Categories', data: categories}, status: :ok
+                render :json => categories, status: :ok
             end
 
             def show
-                #will show the todo for that category instead of showing the category
                 category = Category.find(params[:id])
-                todo = category.todos
-                render json: {status: 'SUCCESS', message: 'Loaded a Category', data: todo}, status: :ok
+                render :json => category, status: :ok
             end
 
             def create
                 category = Category.new(category_params)
                 if category.save
-                    render json: {status: 'SUCCESS', message: 'New Category saved', data: category}, status: :ok
+                    render json: {message: 'New Category saved', data: category}, status: :ok
                 else 
-                    render json: {status: 'FAILED', message: 'New Category not saved', data: category.errors}, status: :unprocessable_entity
+                    render json: {message: 'New Category not saved', data: category.errors}, status: :unprocessable_entity
                 end
             end
 
             def destroy
                 category = Category.find(params[:id])
                 category.destroy
-                render json: {status: 'SUCCESS', message: 'Deleted Category', data: category}, status: :ok
+                render json: {message: 'Deleted Category', data: category}, status: :ok
             end
 
             def update
                  category = Category.find(params[:id])
                  if category.update(category_params)   
-                    render json: {status: 'SUCCESS', message: 'Updated Category', data: category}, status: :ok
+                    render json: {message: 'Updated Category', data: category}, status: :ok
                  else
-                    render json: {status: 'FAILED', message: 'Category not updated', data: category.errors}, status: :unprocessable_entity
+                    render json: {message: 'Category not updated', data: category.errors}, status: :unprocessable_entity
                  end
             end
 
--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -3,37 +3,44 @@ module Api
         class TodosController < ApplicationController
             skip_before_action :verify_authenticity_token, :only => [:create, :update]
             def index
-                todos = Todo.order('created_at');
-                render json: {status: 'SUCCESS', message: 'Loaded all Todo Content', data: todos}, status: :ok
+                vars = request.query_parameters
+                if vars['categoryid'] != nil
+                    queried_category = vars['categoryid']
+                    todos = Todo.where(category_id: queried_category)
+                    render :json => todos, status: :ok
+                else
+                    todos = Todo.order('created_at');
+                    render :json => todos, status: :ok
+                end
             end
 
             def show
                 todo = Todo.find(params[:id])
-                render json: {status: 'SUCCESS', message: 'Loaded Todo Content', data: todo}, status: :ok
+                render :json => todo, status: :ok
             end
 
             def create
                 todo = Todo.new(todo_params)
                 puts todo
                 if todo.save
-                    render json: {status: 'SUCCESS', message: 'Todo Content saved', data: todo}, status: :ok
+                    render json: {message: 'Todo Content saved', data: todo}, status: :ok
                 else 
-                    render json: {status: 'FAILED', message: 'Todo Content not saved', data: todo.errors}, status: :unprocessable_entity
+                    render json: {message: 'Todo Content not saved', data: todo.errors}, status: :unprocessable_entity
                 end
             end
 
             def destroy
                 todo = Todo.find(params[:id])
                 todo.destroy
-                render json: {status: 'SUCCESS', message: 'Deleted Todo', data: todo}, status: :ok
+                render json: {message: 'Deleted Todo', data: todo}, status: :ok
             end
 
             def update
                  todo = Todo.find(params[:id])
                  if todo.update(todo_params)   
-                    render json: {status: 'SUCCESS', message: 'Updated Todo', data: todo}, status: :ok
+                    render json: {message: 'Updated Todo', data: todo}, status: :ok
                  else
-                    render json: {status: 'FAILED', message: 'Todo Content not updated', data: todo.errors}, status: :unprocessable_entity
+                    render json: {message: 'Todo Content not updated', data: todo.errors}, status: :unprocessable_entity
                  end
             end
 
